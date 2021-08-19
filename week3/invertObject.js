17e21c1d2c1ca02131b7a1837f8499facc555e23
tigranha <76497804+tigranha@users.noreply.github.com>
2021-08-19 18:26:04 +0400

    Create invertObject.js

---
+++ week3/invertObject.js
@@ -0,0 +1,15 @@
+function invertObject (obj) {
+
+ let temp = {}
+ 
+ for (let [key, value] of Object.entries(obj)){
+   if (temp.hasOwnProperty(value)){
+       temp[value] = [temp[value],key]
+   } else {
+     temp[value] = key
+   }
+ }
+
+  obj = temp
+  return obj
+}
